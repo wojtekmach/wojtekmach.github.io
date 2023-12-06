c9d3d82aa8fdf49a459f5136b95b0224e963ef58
Wojtek Mach <wojtek@wojtekmach.pl>
2023-12-06 11:26:29 +0100

    up

--- elixir-install.sh
+++ elixir-install.sh
@@ -5,6 +5,7 @@ os=`uname -s`
 case $os in
   Darwin) os=darwin ;;
   Linux) os=linux ;;
+  MINGW64*) os=windows ;;
   *) echo "error: unsupported OS: $os." && exit 1
 esac
 
@@ -41,20 +42,32 @@ main() {
 }
 
 install_otp() {
-  otp_tgz="otp_${otp_version}_${os}_${arch}_ssl_1.1.1s.tar.gz"
-
   if [ ! -d $otp_dir ]; then
-    if [ ! -f $otp_tgz ]; then
-      url="https://github.com/wojtekmach/beam_builds/releases/download/OTP-$otp_version/$otp_tgz"
-      echo "downloading $url"
-      curl --fail -LO $url
-    fi
+    if [ "$os" = "windows" ]; then
+      otp_exe="otp_win64_$otp_version.exe"
 
-    echo "unpacking $otp_tgz to $otp_dir..."
-    mkdir $otp_dir
-    tar xzf $otp_tgz --strip-components 2 -C $otp_dir
-    (cd $otp_dir && ./Install -sasl $PWD)
-    rm $otp_tgz
+      if [ ! -f $otp_exe ]; then
+        url="https://github.com/erlang/otp/releases/download/OTP-$otp_version/$otp_exe"
+        echo "downloading $url"
+        curl --fail -LO $url
+        ./$otp_exe //D=`pwd -W`/$otp_dir
+        rm $otp_exe
+      fi
+    else
+      otp_tgz="otp_${otp_version}_${os}_${arch}_ssl_1.1.1s.tar.gz"
+
+      if [ ! -f $otp_tgz ]; then
+        url="https://github.com/wojtekmach/beam_builds/releases/download/OTP-$otp_version/$otp_tgz"
+        echo "downloading $url"
+        curl --fail -LO $url
+      fi
+
+      echo "unpacking $otp_tgz to $otp_dir..."
+      mkdir $otp_dir
+      tar xzf $otp_tgz --strip-components 2 -C $otp_dir
+      (cd $otp_dir && ./Install -sasl $PWD)
+      rm $otp_tgz
+    fi
   fi
 }
 
